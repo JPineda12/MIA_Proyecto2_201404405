55687e7e2fadb4b9d2644ebbd530e5c979279584
brest12 <jpinedaespino@gmail.com>
2020-12-28 01:04:25 -0600

    Frotend iniciado con Angular Material configurado ya :D

---
+++ Frontend/Santa-Cloud-Platform/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AdminComponent } from './components/home/admin/admin.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
